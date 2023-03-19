a3675f5b4f1e6701acb985756e1f8bda8681cf4a
erdembn <72571747+erdembn@users.noreply.github.com>
2023-03-19 20:39:37 +0300

    adddecode_alien_message

---
+++ decode_alien_message/decodeAlienMessage.js
@@ -0,0 +1,36 @@
+
+const title = ":htraE no od ot ffutS";
+const messages = [
+    "maerc eci yrT",
+    "rewoT leffiE tisiV",
+    "noom eht ot snamuh etacoleR",
+    "egrahc ni stac tuP",
+]
+
+
+
+function reverseString(arr) {
+    arr = arr.split(" ")
+    let newArray = []
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i].split("").reverse().join("")
+        newArray.push(word)
+    }
+
+    return newArray.join(" ")
+}
+
+
+
+function reverseStringsInArray(arr) {
+    let newArray = []
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i].split(" ").reverse().join(" ")
+        newArray.push(word)
+    }
+
+    return newArray.map(sentence => reverseString(sentence))
+}
+
+console.log(reverseString(title));
+console.log(reverseStringsInArray(messages));
